bb40d76efc474d7c4857506cba4908e88aada32d
Akr <dksrlfks6079@naver.com>
2023-03-02 09:29:41 +0900

    fort

--- js/common.js
+++ js/common.js
@@ -4,4 +4,13 @@ $(function () {
         nextArrow: $(".next"),
         prevArrow: $(".prev"),
     })
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $(".top").addClass("on")
+            $(".inner").addClass("on")
+        } else {
+            $(".top").removeClass("on")
+            $(".inner").removeClass("on")
+        }
+    })
 })
